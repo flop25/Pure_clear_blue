8c7a0155ec15b350f4c2574b997b3c7d6cc34141
flop25 <flop25@piwigo.org>
2010-04-08 16:11:44 +0000

    creation of the svn repository of Pure_clear_blue with the content of the formerly flopure/clear_blue
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_clear_blue@5715 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,13 @@
+<?php
+$themeconf = array(
+  'template' => 'floPure',
+  'theme' => 'clear_blue',
+  'template_dir' => 'template/floPure',
+  'icon_dir' => 'template/floPure/icon',
+  'admin_icon_dir' => 'template/floPure/icon/admin',
+  'mime_icon_dir' => 'template/floPure/icon/mimetypes/',
+  'local_head' => '<!-- no theme specific head content -->',
+  'eid' => '143',
+  'version' => '2.0.5',
+);
+?>
