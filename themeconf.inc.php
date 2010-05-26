243a7c6c65b10fe43750f5461a3842d56c5e2f46
flop25 <flop25@piwigo.org>
2010-05-26 12:30:14 +0000

    adding head info in themeconf.inc.php and version 1.0.2
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_clear_blue@6369 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,5 +1,14 @@
 <?php
 /*
+Theme Name: Pure_clear_blue
+Version: 1.0.2
+Description: It was my first style but now this version is compatible for piwigo 2.1 </br> Rounded corners, a peaceful blue... you will like this style
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=383
+Author: Piwigo team
+Author URI: http://piwigo.org
+*/
+
+/*
 $themeconf = array(
   'template' => 'floPure',
   'theme' => 'clear_blue',
