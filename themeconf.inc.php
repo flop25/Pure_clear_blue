98612b8b1bad4483b48db28a5f03e17d556c1e71
flop25 <flop25@piwigo.org>
2011-03-15 20:29:49 +0000

    Massive update ! sprite for icons old icons deleted adding a text for about.php version changed to auto for all themes a few other changes
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_clear_blue@9716 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,26 +1,13 @@
 <?php
 /*
 Theme Name: Pure_clear_blue
-Version: 1.0.2
+Version: auto
 Description: It was my first style but now this version is compatible for piwigo 2.1 </br> Rounded corners, a peaceful blue... you will like this style
 Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=383
 Author: flop25
-Author URI: http://www.planete-flop.fr/site.php?acc=file
+Author URI: http://www.planete-flop.fr/
 */
 
-/*
-$themeconf = array(
-  'template' => 'floPure',
-  'theme' => 'clear_blue',
-  'template_dir' => 'template/floPure',
-  'icon_dir' => 'template/floPure/icon',
-  'admin_icon_dir' => 'template/floPure/icon/admin',
-  'mime_icon_dir' => 'template/floPure/icon/mimetypes/',
-  'local_head' => '<!-- no theme specific head content -->',
-  'eid' => '143',
-  'version' => '2.0.5',
-);
-*/
 $themeconf = array(
   'name'         => 'Pure_clear_blue',
   'parent'        => 'Pure_default',
